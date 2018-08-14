3edd361e1d460ea58691c7ad925a0f9c4a44c650
zmil44 <zachary.j.miller@outlook.com>
2018-08-14 11:24:08 -0400

    add StudentInformation

---
+++ assignment 4-1/Assignment4-1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment4_1
+{
+    class Program
+    {
+ 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\tFirst Test");
+            int studentNumber = 0001234567;
+            string firstName = "John";
+            string lastName = "Logan";
+            double gpa = 3.6;
+            string classification = "Senior";
+            string major = "English";
+            Student newStudent = new Student(studentNumber,firstName,lastName,gpa,classification,major);
+            Console.WriteLine(newStudent);
+            Console.WriteLine("\n\t Another Test");
+            Student student2 = new Student(0001222876, "Chin", "Chang", 3.7, "Junior", "CS");
+            Console.WriteLine(student2);
+        }
+    }
+}
---
+++ assignment 4-1/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment4_1
+{
+    class Student
+    {
+        int studentNumber;
+        string firstName;
+        string lastName;
+        double gpa;
+        string classification;
+        string major;
+
+        public Student()
+        {
+        }
+
+        public Student(int id, string fName, string lName, double grade_Point_Average, string grade, string maj)
+        {
+            studentNumber = id;
+            firstName = fName;
+            lastName = lName;
+            gpa = grade_Point_Average;
+            classification = grade;
+            major = maj;
+        }
+
+        public override string ToString()
+        {
+            return "Student Number: " + studentNumber + "\nFirst Name: " + firstName + "\nLast Name: " + lastName + "\nGPA: " + gpa +
+                "\nClassification: " + classification + "\nMajor: " + major;
+            //return base.ToString();
+        }
+    }
+}
